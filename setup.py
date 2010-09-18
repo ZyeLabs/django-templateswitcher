2b975dc6cef86359e858fa007d54b7c938ec582e
jonathan <jonathan@qoda-server.(none)>
2010-09-18 09:28:47 +0200

    modiifed package requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='http://github.com/praekelt/django-templateswitcher',
     packages = find_packages(),
     install_requires = [
-        'pywurfl',
+        'djangobile',
     ],
     include_package_data=True,
     classifiers = [
