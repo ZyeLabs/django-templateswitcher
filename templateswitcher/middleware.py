418c433e3eae540ed9d3332ec54a11737a6c3805
jonathan <jonathan@qoda-server.(none)>
2010-09-22 11:49:40 +0200

    modified to test for existing device in request object

--- templateswitcher/middleware.py
+++ templateswitcher/middleware.py
@@ -4,7 +4,6 @@ from django.conf import settings
 
 from mobile.sniffer.chain import ChainedSniffer
 
-
 DEVICE_FAMILIES = getattr(settings, 'DEVICE_FAMILIES', 'templateswitcher.device_families')
 device_families = __import__(DEVICE_FAMILIES)
 
@@ -19,35 +18,44 @@ class TemplateDirSwitcher(object):
     """
     def process_request(self, request):
         
-        # set the device switcher library according to the settings - defaults to wurfl
-        device_switch_libs = getattr(settings, 'DEVICE_SWITCH_LIB', ['WurlfSniffer'])
-        da_api = getattr(settings, 'DEVICE_ATLAS_API_FILE', None)
+        if request.device:
+            template_set = get_device_family(request.device)
             
-        # import requied device library classes
-        if 'ApexVertexSniffer' in device_switch_libs:
-            from mobile.sniffer.apexvertex.sniffer import ApexVertexSniffer
-        if 'WAPProfileSniffer' in device_switch_libs:
-            from mobile.sniffer.wapprofile.sniffer import WAPProfileSniffer
-        if 'DeviceAtlasSniffer' in device_switch_libs:
-            if not os.path.exists(da_api):
-                raise Exception('DEVICE_ATLAS_API_FILE must be in your setting and contain a valid path to use Device Atlas.')
-            from mobile.sniffer.deviceatlas.sniffer import DeviceAtlasSniffer
-        if 'WurlfSniffer' in device_switch_libs:
-            from mobile.sniffer.wurlf.sniffer import WurlfSniffer
+            # switch the template dir for the given device
+            settings.TEMPLATE_DIRS = (
+                settings.DEVICE_TEMPLATE_DIRS[template_set],
+            )
         
-        chained_libs = []
-        for device_lib in device_switch_libs:
-            chained_libs.append(eval(device_lib)())
-        
-        # instantiate the sniffer and device object
-        sniffer = ChainedSniffer(chained_libs)
-        device_object = sniffer.sniff(request)
-        template_set = get_device_family(device_object)
+        else:
+            # set the device switcher library according to the settings - defaults to wurfl
+            device_switch_libs = getattr(settings, 'DEVICE_SWITCH_LIB', ['WurlfSniffer'])
+            da_api = getattr(settings, 'DEVICE_ATLAS_API_FILE', None)
+                
+            # import requied device library classes
+            if 'ApexVertexSniffer' in device_switch_libs:
+                from mobile.sniffer.apexvertex.sniffer import ApexVertexSniffer
+            if 'WAPProfileSniffer' in device_switch_libs:
+                from mobile.sniffer.wapprofile.sniffer import WAPProfileSniffer
+            if 'DeviceAtlasSniffer' in device_switch_libs:
+                if not os.path.exists(da_api):
+                    raise Exception('DEVICE_ATLAS_API_FILE must be in your setting and contain a valid path to use Device Atlas.')
+                from mobile.sniffer.deviceatlas.sniffer import DeviceAtlasSniffer
+            if 'WurlfSniffer' in device_switch_libs:
+                from mobile.sniffer.wurlf.sniffer import WurlfSniffer
             
-        # copy the device object to the request object
-        request.device = device_object
-        
-        # switch the template dir for the given device
-        settings.TEMPLATE_DIRS = (
-            settings.DEVICE_TEMPLATE_DIRS[template_set],
-        )
+            chained_libs = []
+            for device_lib in device_switch_libs:
+                chained_libs.append(eval(device_lib)())
+            
+            # instantiate the sniffer and device object
+            sniffer = ChainedSniffer(chained_libs)
+            device_object = sniffer.sniff(request)
+            template_set = get_device_family(device_object)
+                
+            # copy the device object to the request object
+            request.device = device_object
+            
+            # switch the template dir for the given device
+            settings.TEMPLATE_DIRS = (
+                settings.DEVICE_TEMPLATE_DIRS[template_set],
+            )
