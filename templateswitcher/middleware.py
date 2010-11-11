df559161d36068136cb6ae5d5bb28176dd3a7b3d
Ismail Dhorat <ismail@zyelabs.net>
2010-11-11 18:23:40 +0200

    check cache first before running device query

--- templateswitcher/middleware.py
+++ templateswitcher/middleware.py
@@ -2,6 +2,7 @@ import importlib
 import os
 
 from django.conf import settings
+from django.core.cache import cache
 
 from mobile.sniffer.chain import ChainedSniffer
 
@@ -15,11 +16,18 @@ class TemplateDirSwitcher(object):
     def process_request(self, request):
         device_families = importlib.import_module(getattr(settings, 'DEVICE_FAMILIES', 'templateswitcher.device_families'))
         device_obj = getattr(request, 'device', None)
+        device_cache_key = request.META['HTTP_USER_AGENT']
+        device_cache_key = device_cache_key.replace(' ','')
+        template_set = cache.get(device_cache_key)
+        full_path = request.get_full_path()
+        media_request = (full_path.startswith(settings.MEDIA_URL) or
+                        full_path.startswith(settings.ADMIN_MEDIA_PREFIX) or full_path.startswith('/favicon'))
         
-        if not device_obj:
+        if not template_set:
             # set the device switcher library according to the settings - defaults to wurfl
             device_switch_libs = getattr(settings, 'DEVICE_SWITCH_LIB', ['WurlfSniffer'])
             da_api = getattr(settings, 'DEVICE_ATLAS_API_FILE', None)
+            device_cache_timeout = getattr(settings, 'DEVICE_CACHE_TIMEOUT', 72000)
                 
             # import requied device library classes
             if 'ApexVertexSniffer' in device_switch_libs:
@@ -44,7 +52,9 @@ class TemplateDirSwitcher(object):
                 
             # copy the device object to the request object
             request.device = device_object
+            cache.set(device_cache_key, template_set, device_cache_timeout)
             
+        if not media_request:
             # switch the template dir for the given device
             settings.TEMPLATE_DIRS = (
                 settings.DEVICE_TEMPLATE_DIRS[template_set],
